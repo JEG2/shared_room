c62023c0f5351cd4e40cd3f2d53100c0740c21e6
James Edward Gray II <james@graysoftinc.com>
2018-11-30 18:21:43 -0600

    mix scenic.new

---
+++ mix.exs
@@ -0,0 +1,30 @@
+defmodule SharedRoom.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :shared_room,
+      version: "0.1.0",
+      elixir: "~> 1.7",
+      build_embedded: true,
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      mod: {SharedRoom, []},
+      extra_applications: []
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:scenic, "~> 0.9"},
+      {:scenic_driver_glfw, "~> 0.9"},
+    ]
+  end
+end
