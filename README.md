d654c2e7bab275e624a2f0b841e2f85c10e28340
James Edward Gray II <james@graysoftinc.com>
2018-11-30 22:30:26 -0600

    Attempting multiplayer.

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
-Readme text goes here
-
-
-From template
+Host:  elixir --name 'room@192.168.1.139' --cookie 'beamnetworkingftw' -S mix run -e 'Application.ensure_started(:shared_room)' --no-halt -- HOST
+Client:  elixir --name 'room@192.168.1.141' --cookie 'beamnetworkingftw' -S mix run -e 'Application.ensure_started(:shared_room)' --no-halt -- 'room@192.168.1.139'
--- lib/shared_room.ex
+++ lib/shared_room.ex
@@ -4,21 +4,41 @@ defmodule SharedRoom do
   """
 
   def start(_type, _args) do
-    import Supervisor.Spec, warn: false
-
     # load the viewport configuration from config
     main_viewport_config = Application.get_env(:shared_room, :viewport)
+
+    Supervisor.start_link(
+      children(System.argv, main_viewport_config),
+      strategy: :one_for_one
+    )
+  end
+
+  defp children(["HOST"], main_viewport_config) do
     cells =
       main_viewport_config
       |> Map.fetch!(:size)
       |> SharedRoom.Grid.pixels_to_cells
 
-    # start the application with the viewport
-    children = [
+    [
       {SharedRoom.Room, cells},
-      supervisor(Scenic, viewports: [main_viewport_config])
+      scenic_child(main_viewport_config)
     ]
+  end
+  defp children([address], main_viewport_config) do
+    Node.connect(String.to_atom(address))
+    [
+      scenic_child(main_viewport_config)
+    ]
+  end
+  defp children(_args, _main_viewport_config) do
+    IO.puts "See README"
+    System.halt
+  end
 
-    Supervisor.start_link(children, strategy: :one_for_one)
+  defp scenic_child(main_viewport_config) do
+    import Supervisor.Spec, warn: false
+
+    # start the application with the viewport
+    supervisor(Scenic, viewports: [main_viewport_config])
   end
 end
